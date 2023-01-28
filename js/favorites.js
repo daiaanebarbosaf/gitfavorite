a5eec3382c5e05fe21c089ae04bb31b25d9856c5
daiaanebarbosaf <daiaanebarbosaf@gmail.com>
2023-01-28 17:50:59 -0300

    🎉 projeto finalizado

--- js/favorites.js
+++ js/favorites.js
@@ -4,12 +4,10 @@ export class Favorites {
   constructor(root){
     this.root = document.querySelector(root)
     this.load()
-    
   }
 
   load() {
     this.entries = JSON.parse(localStorage.getItem('@github-favorites:')) || []
-
   }
 
   save() {
@@ -37,7 +35,6 @@ export class Favorites {
     } catch(error) {
         alert(error.message)
     }
-
   }
 
   delete(user) {
